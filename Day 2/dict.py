628b4e7d75a7d39c715da72fbb7649642fddb7bd
Manish Gautam <itsmanishgautam@gmail.com>
2025-06-10 23:10:49 +0545

    simple dict python

--- Day 2/dict.py
+++ Day 2/dict.py
@@ -1,26 +1,3 @@
-temp_memory = {}
-
-def store_data(key, value):
-    temp_memory[key] = value
-    print(f"Stored: {key} → {value}")
-
-def get_data(key):
-    if key in temp_memory:
-        print(f"Retrieved: {key} → {temp_memory[key]}")
-    else:
-        print(f"Key '{key}' not found in memory.")
-
-def show_memory():
-    print("Current Memory:")
-    for key, value in temp_memory.items():
-        print(f"  {key} → {value}")
-
-store_data("name", "Manish")
-store_data("age", 25)
-get_data("name")
-get_data("city")
-show_memory()
-
 student ={
     "name":"manish",
     "class":"math",
@@ -37,3 +14,52 @@ print(student)
 
 del student["name"]
 print(student)
+
+
+employees = {}
+
+def add_employee(key, value):
+    employees[key] = value
+    print(f"stored {key} = {value}")
+
+
+print("Add Employee Data")
+key1 = input(str("Enter Key Value "))
+value1 = input(str("Enter Value "))
+
+add_employee(key1, value1)
+
+
+
+def display_employee():
+    for key,value in employees:
+        print(f"{key}={value}")
+
+
+
+
+
+
+
+# temp_memory = {}
+
+# def store_data(key, value):
+#     temp_memory[key] = value
+#     print(f"Stored: {key} → {value}")
+
+# def get_data(key):
+#     if key in temp_memory:
+#         print(f"Retrieved: {key} → {temp_memory[key]}")
+#     else:
+#         print(f"Key '{key}' not found in memory.")
+
+# def show_memory():
+#     print("Current Memory:")
+#     for key, value in temp_memory.items():
+#         print(f"  {key} → {value}")
+
+# store_data("name", "Manish")
+# store_data("age", 25)
+# get_data("name")
+# get_data("city")
+# show_memory()
